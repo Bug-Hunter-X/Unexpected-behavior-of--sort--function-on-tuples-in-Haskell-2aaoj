6499c497c61301680f0329cd13887ffb29b48490
BugHunter-X <askzenai@gmail.com>
2025-02-10 23:24:07 +0100

    Add bug.hs file

---
+++ bug.hs
@@ -0,0 +1,16 @@
+```haskell
+import Data.List (sort)
+
+main :: IO ()
+main = do
+  let xs = [1, 2, 3, 4, 5]
+  let ys = sort xs
+  print ys -- Output: [1,2,3,4,5]
+  let zs = [5,4,3,2,1]
+  let ws = sort zs
+  print ws -- Output: [1,2,3,4,5]
+
+  let as = [(1, "a"), (2, "b"), (3, "c")]
+  let bs = sort as --Error occurs here
+  print bs
+```
